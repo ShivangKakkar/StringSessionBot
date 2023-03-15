6ef0d1d0287a8cb14076288b5a09c63262b62930
Snehashish Biswas <teamscenariox@gmail.com>
2023-03-15 06:06:47 +0530

    ⚡

--- StringSessionBot/database/__init__.py
+++ StringSessionBot/database/__init__.py
@@ -1,18 +1,20 @@
-from env import DATABASE_URL
-
-if DATABASE_URL == "":
-    return
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
+from env import DATABASE_URL
+
+count_ = 0
 def start() -> scoped_session:
+    if DATABASE_URL == "":
+        if count_ < 1:
+            return print("Database url not provided..\nBut this time I won't stop 😉")
+    return
     engine = create_engine(DATABASE_URL)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-
-BASE = declarative_base()
-SESSION = start()
+if DATABASE_URL != "":
+    BASE = declarative_base()
+    SESSION = start()
