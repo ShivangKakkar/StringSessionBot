4e0a7dcbfdced7719ff6b4d385a9fdf3f8d8b992
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2023-03-20 04:32:14 +0700

    fixed invalid syntax

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -50,12 +50,10 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             if query == "pyrogram":
                 await callback_query.answer("Please note that the new type of string sessions may not work in all bots, i.e, only the bots that have been updated to pyrogram v2 will work!", show_alert=True)
                 await generate_session(bot, callback_query.message)
-            """
             # Maybe in future it'll come back.
-            elif query == "pyrogram1":
-                await callback_query.answer()
-                await generate_session(bot, callback_query.message, old_pyro=True)
-            """
+            #elif query == "pyrogram1":
+            #    await callback_query.answer()
+            #    await generate_session(bot, callback_query.message, old_pyro=True)
             elif query == "pyrogram_bot":
                 await callback_query.answer("Please note that this bot session will be of pyrogram v2", show_alert=True)
                 await generate_session(bot, callback_query.message, is_bot=True)
