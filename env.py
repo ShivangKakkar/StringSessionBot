8741a8bb63e6c4271e370875d74544445a8f8a5c
Snehashish <bsantanu990@gmail.com>
2023-03-21 09:55:35 +0530

    update

--- env.py
+++ env.py
@@ -10,23 +10,21 @@ DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
 MUST_JOIN = os.getenv("MUST_JOIN", "")
 
 if not API_ID:
-    print("No API_ID found. Exiting...")
-    raise SystemExit
-if not API_HASH:
-    print("No API_HASH found. Exiting...")
-    raise SystemExit
-if not BOT_TOKEN:
-    print("No BOT_TOKEN found. Exiting...")
-    raise SystemExit
+    raise SystemExit("No API_ID found. Exiting...")
+elif not API_HASH:
+    raise SystemExit("No API_HASH found. Exiting...")
+elif not BOT_TOKEN:
+    raise SystemExit("No BOT_TOKEN found. Exiting...")
+'''
 if not DATABASE_URL:
     print("No DATABASE_URL found. Exiting...")
     raise SystemExit
+'''
 
 try:
     API_ID = int(API_ID)
 except ValueError:
-    print("API_ID is not a valid integer. Exiting...")
-    raise SystemExit
+    raise SystemExit("API_ID is not a valid integer. Exiting...")
 
 if 'postgres' in DATABASE_URL and 'postgresql' not in DATABASE_URL:
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
